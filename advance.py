b161a5e85939c2e728dca43f3a35ded4d672cf30
shammas01 <shammasmhd0@gmail.com>
2023-10-09 20:30:27 +0530

    edited

--- advance.py
+++ advance.py
@@ -578,29 +578,29 @@ print(name('Shammas'))
 
 # #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
-# def factorial(n):
+def factorial(n):
 
-#     def inner_factorial(y):
+    def inner_factorial(y):
             
-#         def super_inner(n):
-#             def very_inner(x):
+        def super_inner(n):
+            def very_inner(x):
                     
-#                 if x==0:
-#                     return 1
-#                 else:
+                if x==0:
+                    return 1
+                else:
             
-#                     return x * factorial(x-1)
-#             return very_inner(n)
+                    return x * factorial(x-1)
+            return very_inner(n)
                 
-#         return super_inner(y)
+        return super_inner(y)
            
 
-#     if type(n) == int and n>=0:
-#         return inner_factorial(int(n))          
-#     else:
-#         return TypeError(" 'n' has to be a intiger and should a positive number")   
+    if type(n) == int and n>=0:
+        return inner_factorial(int(n))          
+    else:
+        return TypeError(" 'n' has to be a intiger and should a positive number")   
 
-# print(factorial(13))
+print(factorial(13))
 
 
 
